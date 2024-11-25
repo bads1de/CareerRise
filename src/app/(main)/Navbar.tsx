8259f7d2bc50ea70b92a19de296efd9c9b52d64c
s3xmaxman <badside3919@gmail.com>
2024-11-25 15:47:30 +0900

    update Navbar.tsx and layout.tsx

--- src/app/(main)/Navbar.tsx
+++ src/app/(main)/Navbar.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import logo from "@/assets/logo.png";
+import ThemeToggle from "@/components/ThemeToggle";
 import { UserButton } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
+import { useTheme } from "next-themes";
+import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
+  const { theme } = useTheme();
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -20,24 +24,28 @@ export default function Navbar() {
           />
           <span className="text-xl font-bold tracking-tight">CareerRise</span>
         </Link>
-        <UserButton
-          appearance={{
-            elements: {
-              avatarBox: {
-                width: 35,
-                height: 35,
+        <div className="flex items-center gap-3">
+          <ThemeToggle />
+          <UserButton
+            appearance={{
+              baseTheme: theme === "dark" ? dark : undefined,
+              elements: {
+                avatarBox: {
+                  width: 35,
+                  height: 35,
+                },
               },
-            },
-          }}
-        >
-          <UserButton.MenuItems>
-            <UserButton.Link
-              label="Billing"
-              labelIcon={<CreditCard className="size-4" />}
-              href="/billing"
-            />
-          </UserButton.MenuItems>
-        </UserButton>
+            }}
+          >
+            <UserButton.MenuItems>
+              <UserButton.Link
+                label="Billing"
+                labelIcon={<CreditCard className="size-4" />}
+                href="/billing"
+              />
+            </UserButton.MenuItems>
+          </UserButton>
+        </div>
       </div>
     </header>
   );
