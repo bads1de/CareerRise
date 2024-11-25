a2768eb0e06625964d0fe922e2c286cf01ce7318
s3xmaxman <badside3919@gmail.com>
2024-11-25 20:35:38 +0900

    update ResumeEditor.tsx and steps.ts

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -2,24 +2,39 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import GeneralInfoForm from "./forms/GeneralInfoForm";
-import PersonalInfoForm from "./forms/PersonalInfoForm";
+import { useSearchParams } from "next/navigation";
+import { steps } from "./steps";
+import Breadcrumbs from "./Breadcrumbs";
 
 export default function ResumeEditor() {
+  const searchParams = useSearchParams();
+
+  const currentStep = searchParams.get("step") || steps[0].key;
+
+  const setStep = (key: string) => {
+    const newSearchParams = new URLSearchParams(searchParams.toString());
+    newSearchParams.set("step", key);
+    window.history.pushState(null, "", `?${newSearchParams.toString()}`);
+  };
+
+  const FormComponent = steps.find(
+    (step) => step.key === currentStep,
+  )?.component;
+
   return (
     <div className="flex grow flex-col">
       <header className="space-y-1.5 border-b px-3 py-5 text-center">
-        <h1 className="text-xl font-bold">Design your resume</h1>
+        <h1 className="text-xl font-bold">履歴書を作成</h1>
         <p className="text-sm text-muted-foreground">
-          Follow the steps below to create your resume. Your progress will be
-          saved automatically.
+          履歴書を作成してください。進捗は自動的に保存されます。
         </p>
       </header>
 
       <main className="relative grow">
         <div className="absolute bottom-0 top-0 flex w-full">
-          <div className="w-full overflow-auto p-3 md:w-1/2">
-            <PersonalInfoForm />
+          <div className="w-full space-y-6 overflow-auto p-3 md:w-1/2">
+            <Breadcrumbs currentStep={currentStep} setCurrentStep={setStep} />
+            {FormComponent && <FormComponent />}
           </div>
           <div className="grow md:border-r" />
           <div className="hidden w-1/2 md:flex">right</div>
---
+++ src/app/(main)/editor/steps.ts
@@ -0,0 +1,11 @@
+import GeneralInfoForm from "./forms/GeneralInfoForm";
+import PersonalInfoForm from "./forms/PersonalInfoForm";
+
+export const steps: {
+  title: string;
+  component: React.ComponentType;
+  key: string;
+}[] = [
+  { title: "基本情報", component: GeneralInfoForm, key: "general-info" },
+  { title: "個人情報", component: PersonalInfoForm, key: "personal-info" },
+];
