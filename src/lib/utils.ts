a0a32afc2f654cf4cde373dbe1a51ac4b3541f97
s3xmaxman <badside3919@gmail.com>
2024-11-27 13:26:00 +0900

    update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+/**
+ * ファイルオブジェクトを単純なオブジェクトに変換する関数
+ *
+ * @param key - オブジェクトのキー（この関数では使用されませんが、JSON.stringifyのreplacer関数の形式に合わせるため含まれています）
+ * @param value - 変換対象の値
+ * @returns 変換された値。Fileオブジェクトの場合は単純なオブジェクトに、それ以外の場合は元の値をそのまま返します
+ */
 export function fileReplacer(key: unknown, value: unknown) {
   return value instanceof File
     ? {
