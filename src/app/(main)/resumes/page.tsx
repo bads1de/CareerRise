a7cc19d91b8b51565444dc7702c97fe1969daec3
s3xmaxman <badside3919@gmail.com>
2024-11-28 12:22:00 +0900

    create 1 file and update 1 file

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -2,12 +2,31 @@ import { Button } from "@/components/ui/button";
 import { PlusSquare } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { resumeDataInclude } from "@/lib/types";
+import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
   title: "Resumes",
 };
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const [resumes, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: { userId },
+      orderBy: { createdAt: "desc" },
+      include: resumeDataInclude,
+    }),
+    prisma.resume.count({ where: { userId } }),
+  ]);
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
       <Button asChild className="mx-auto flex w-fit gap-2">
@@ -16,6 +35,15 @@ export default function Page() {
           新規作成
         </Link>
       </Button>
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">あなたの履歴書</h1>
+        <p>合計: {totalCount}</p>
+      </div>
+      <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {resumes.map((resume) => (
+          <ResumeItem key={resume.id} resume={resume} />
+        ))}
+      </div>
     </main>
   );
 }
